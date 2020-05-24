ea1a6ba9326b09541708972b6884ca7d6c8734ad
Ricardo Rossi <solucoessw@gmail.com>
2020-05-24 18:53:43 -0300

    Implementada a função para adicionar a BOM na folha ativa.
    Eliminado o traverse feature para checar se a BOM existe na folha. A checagem agora é feita pelo GetKeepLinkedBOMName que pega o nome da BOM vinculada com primeira vista do desenho.

--- MeuTaskPane.cs
+++ MeuTaskPane.cs
@@ -5,77 +5,58 @@ using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 
-namespace Taskpane_ZeroGrau
-{
+namespace Taskpane_ZeroGrau {
     [ComVisible(true)]
     [ProgId("TaskPane2")]
-    public partial class MeuTaskPane : UserControl
-    {
+    public partial class MeuTaskPane : UserControl {
         ISldWorks swApp;
-        public MeuTaskPane()
-        {
+        public MeuTaskPane() {
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
+        private void button1_Click(object sender, EventArgs e) {
             SldWorks swApp = SolidWorksSingleton.GetApplication();
             ModelDoc2 swModel;
-
+            ModelDocExtension swModelExt;
+            FeatureManager swFeatureMgr;
             swModel = (ModelDoc2)swApp.ActiveDoc;
+            swModelExt = swModel.Extension;
 
-            if (!(GetBomTable(swModel)))
-            {
-                InsertBOM(swModel);
-            }
+            InsertBOM(swModel);
+            swFeatureMgr = swModel.FeatureManager;
+            swFeatureMgr.UpdateFeatureTree();
         }
 
-        public void GetSwApp(ISldWorks swAppIn)
-        {
+        public void GetSwApp(ISldWorks swAppIn) {
             swApp = swAppIn;
         }
 
-        private static bool GetBomTable(ModelDoc2 swModel)
-        {
-            Feature swFeature;
-            //BomFeature swBomFeat;
-            swFeature = swModel.FirstFeature();
-            while (!(swFeature == null))
-            {
-                string featureName = swFeature.GetTypeName2();
-                if (featureName == "BomFeat")
-                {
-                    return true;
-                }
-                swFeature = swFeature.GetNextFeature();
-            }
-            return false;
-        }
-
-        private static void InsertBOM(ModelDoc2 swmodel)
-        {
+        private static void InsertBOM(ModelDoc2 swmodel) {
             DrawingDoc swDraw = (DrawingDoc)swmodel;
+            Sheet currentSheet = swDraw.GetCurrentSheet();
+            var views = currentSheet.GetViews();
             SolidWorks.Interop.sldworks.View swSheetView;
-            SolidWorks.Interop.sldworks.View swActiveView;
-            swSheetView = swDraw.GetFirstView(); // De fato é a folha, por isso a primeira vista é obtida por GetNextView
-            swActiveView = swSheetView.GetNextView();
-            swmodel = swActiveView.ReferencedDocument;
-            string config = swActiveView.ReferencedConfiguration;
+            swSheetView = views[0];
 
-            var bomPart = @"C:\Users\Ricar\Documents\Add-in Ricardo\Taskpane_ZeroGrau\bin\Release\PART.sldbomtbt";
-            var bomAssembly = @"C:\Users\Ricar\Documents\Add-in Ricardo\Taskpane_ZeroGrau\bin\Release\ASSEMBLY.sldbomtbt";
+            //Check if the view is linked to a BOM
+            //var exiteBOM = swSheetView.GetKeepLinkedToBOM();
+            var bomName = swSheetView.GetKeepLinkedToBOMName();
+            if (String.IsNullOrEmpty(bomName) ) {
+                swmodel = swSheetView.ReferencedDocument;
+                string config = swSheetView.ReferencedConfiguration;
 
-            if ((swDocumentTypes_e)swmodel.GetType() == swDocumentTypes_e.swDocASSEMBLY)
-            {
-                swActiveView.InsertBomTable4(false, 0.413, 0.049, (int)swBOMConfigurationAnchorType_e.swBOMConfigurationAnchor_BottomRight,
-                   (int)swBomType_e.swBomType_TopLevelOnly, config, bomAssembly, false, (int)swNumberingType_e.swNumberingType_None, false);
+                var bomPart = @"C:\Users\Ricar\Documents\Add-in Ricardo\Taskpane_ZeroGrau\bin\Release\PART.sldbomtbt";
+                var bomAssembly = @"C:\Users\Ricar\Documents\Add-in Ricardo\Taskpane_ZeroGrau\bin\Release\ASSEMBLY.sldbomtbt";
+
+                if ((swDocumentTypes_e)swmodel.GetType() == swDocumentTypes_e.swDocASSEMBLY) {
+                    swSheetView.InsertBomTable4(false, 0.413, 0.049, (int)swBOMConfigurationAnchorType_e.swBOMConfigurationAnchor_BottomRight,
+                       (int)swBomType_e.swBomType_TopLevelOnly, config, bomAssembly, false, (int)swNumberingType_e.swNumberingType_None, false);
+                }
+                else if ((swDocumentTypes_e)swmodel.GetType() == swDocumentTypes_e.swDocPART) {
+                    swSheetView.InsertBomTable4(false, 0.413, 0.049, (int)swBOMConfigurationAnchorType_e.swBOMConfigurationAnchor_BottomRight,
+                       (int)swBomType_e.swBomType_PartsOnly, config, bomPart, false, (int)swNumberingType_e.swNumberingType_None, false);
+                }
             }
-            else if ((swDocumentTypes_e)swmodel.GetType() == swDocumentTypes_e.swDocPART)
-            {
-                swActiveView.InsertBomTable4(false, 0.413, 0.049, (int)swBOMConfigurationAnchorType_e.swBOMConfigurationAnchor_BottomRight,
-                   (int)swBomType_e.swBomType_PartsOnly, config, bomPart, false, (int)swNumberingType_e.swNumberingType_None, false);
-            }
-
         }
     }
 }
